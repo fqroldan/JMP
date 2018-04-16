e25dc36439aae174a7054919d5c65579a0cadcc3
qurufinwe <fr673@nyu.edu>
2018-04-16 09:20:32 -0400

    Added local_run functionality

--- main.jl
+++ main.jl
@@ -24,23 +24,28 @@ print_save("\nAggregate Demand around Debt Crises\n")
 
 print_save("\nStarting $(location) run on $(nprocs()) cores at "*Dates.format(now(), "HH:MM"))
 
+# Set options
+local_run = false
+
 # Initialize type
-h = Hank();
-# h = load("hank.jld", "h")
-try
-	h2 = load("hank.jld", "h")
-	if h.ψ == h2.ψ && h.γ == h2.γ && h.Ns == h2.Ns
-		h.ϕa = h2.ϕa
-		h.ϕb = h2.ϕb
-		h.ϕc = h2.ϕc
-		h.vf = h2.vf
-		h.pN = h2.pN
-		h.wage = h2.wage
-		h.Ld = h2.Ld
+if remote || local_run == false
+	h = Hank();
+	try
+		h2 = load("hank.jld", "h")
+		if h.ψ == h2.ψ && h.γ == h2.γ && h.Ns == h2.Ns
+			h.ϕa = h2.ϕa
+			h.ϕb = h2.ϕb
+			h.ϕc = h2.ϕc
+			h.vf = h2.vf
+			h.pN = h2.pN
+			h.wage = h2.wage
+			h.Ld = h2.Ld
+		end
 	end
+else
+	h = load("../HPC_Output/hank.jld", "h")
 end
 
-# h = load("../HPC_Output/hank.jld", "h")
 
 print_save("\nβ, RRA, IES: $(round(h.β,2)), $(h.γ), $(h.ψ)")
 print_save("\nϵ: $(h.ϵgrid)")
@@ -48,8 +53,10 @@ print_save("\nz: $(h.zgrid)")
 print_save("\nω: $(h.ωgrid)\n")
 
 # Run
-vfi!(h, verbose = true, remote = remote)
-save(pwd() * "/../../hank.jld", "h", h)
+if remote || local_run 
+	vfi!(h, verbose = true, remote = remote)
+	save(pwd() * "/../../hank.jld", "h", h)
+end
 
 p, jz_series = simul(h; simul_length=200, burn_in=100, only_def_end=true)
 plot_simul(p; remote=remote)
