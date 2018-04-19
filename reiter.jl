69b3d4e6dbb19e10a6f411f16c2e989e52e826f6
qurufinwe <fr673@nyu.edu>
2018-04-19 09:28:53 -0400

    More discounting

--- reiter.jl
+++ reiter.jl
@@ -2,7 +2,7 @@ using QuantEcon, BasisMatrices, Interpolations, Optim, MINPACK, LaTeXStrings, Di
 
 include("hh_pb.jl")
 
-function Hank(;	β = (1.0/1.15)^0.25,
+function Hank(;	β = (1.0/1.05)^0.25,
 				IES = 2.0,
 				RRA = 20.,
 				γw = 0.99^0.25,
