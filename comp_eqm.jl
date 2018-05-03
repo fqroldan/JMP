8cad49c0b2bb0e5449ff1dfe4681efcb57ca8adc
qurufinwe <fr673@nyu.edu>
2018-05-03 08:21:14 -0400

    Reintroduced cubature

--- comp_eqm.jl
+++ comp_eqm.jl
@@ -136,32 +136,32 @@ function mkt_clearing(h::Hank, itp_ϕc, G, Bpv, pNv, pNmin, pNmax, bv, μv, σv,
 
 	# Step 3: Get the household's policies at these prices
 
-	# val_int_C = 0.
-	# for (jϵ, ϵv) in enumerate(h.ϵgrid)
-
-	# 	f(ω) = pdf(LogNormal(μv, σv), ω-h.ωmin) * h.λϵ[jϵ] * itp_ϕc[ω, jϵ, bv, μv, σv, wv, jζ, jz, pN]
-
-	# 	(val, err) = hquadrature(f, h.ωmin, h.ωmax,
-	# 								reltol=1e-8, abstol=0, maxevals=0)
-
-	# 	val_int_C += val
-	# end
-	val_C, sum_prob = 0., 0.
+	val_int_C = 0.
 	for (jϵ, ϵv) in enumerate(h.ϵgrid)
-		for jω = 1:length(h.ωgrid_fine)-1
-			ωv  = h.ωgrid_fine[jω]
-			ω1v = h.ωgrid_fine[jω+1]
-			ωmv = 0.5*(ωv+ω1v)
 
-			prob = pdf(LogNormal(μv, σv), ωmv-h.ωmin) * h.λϵ[jϵ] * (ω1v - ωv)
+		f(ω) = pdf(LogNormal(μv, σv), ω-h.ωmin) * h.λϵ[jϵ] * itp_ϕc[ω, jϵ, bv, μv, σv, wv, jζ, jz, pN]
 
-			ϕc = itp_ϕc[ωmv, jϵ, bv, μv, σv, wv, jζ, jz, pN]
+		(val, err) = hquadrature(f, h.ωmin, h.ωmax,
+									reltol=1e-8, abstol=0, maxevals=0)
 
-			val_C  += prob * ϕc
-			sum_prob += prob
-		end
+		val_int_C += val
 	end
-	val_int_C = val_C / sum_prob
+	# val_C, sum_prob = 0., 0.
+	# for (jϵ, ϵv) in enumerate(h.ϵgrid)
+	# 	for jω = 1:length(h.ωgrid_fine)-1
+	# 		ωv  = h.ωgrid_fine[jω]
+	# 		ω1v = h.ωgrid_fine[jω+1]
+	# 		ωmv = 0.5*(ωv+ω1v)
+
+	# 		prob = pdf(LogNormal(μv, σv), ωmv-h.ωmin) * h.λϵ[jϵ] * (ω1v - ωv)
+
+	# 		ϕc = itp_ϕc[ωmv, jϵ, bv, μv, σv, wv, jζ, jz, pN]
+
+	# 		val_C  += prob * ϕc
+	# 		sum_prob += prob
+	# 	end
+	# end
+	# val_int_C = val_C / sum_prob
 
 	# Step 4: Check market clearing for nontradables
 	Ld_N, _  = labor_demand(h, w_new, zv, ζv, pN; get_both=true)
