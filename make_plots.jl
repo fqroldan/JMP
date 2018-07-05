bedee3c57a031e46ad7f4eb0fd9876bb98e3c1f9
qurufinwe <fr673@nyu.edu>
2018-07-05 11:34:06 -0400

    bug fix

--- make_plots.jl
+++ make_plots.jl
@@ -1,22 +1,11 @@
 using PlotlyJS, JLD, LaTeXStrings, Rsvg, Interpolations
-# using Plots, JLD, LaTeXStrings, Rsvg
-# plotlyjs()
-
-# include("type_def.jl")
-# h = load(pwd() * "/../HPC_Output/hank.jld", "h")
-
-# include("plotting_routines.jl")
-# plot_state_funcs(h, remote=false)
-# plot_LoM(h, remote=false)
-# plot_labor_demand(h, remote=false)
-# plot_hh_policies(h, remote=false)
 
 p_names = ["conv" "hh" "hh_def" "statefuncs" "LoMs" "nontradables_B" "nontradables_z" "labordemand" "simul" "simul_full" "objfunc" "reactions" "aggcons" "varcons" "outconv"]
 
 for name in p_names
 	print("$name")
 	try
-		if name == "varcons" || name == "nontradables_z"
+		if name == "varcons"
 			p = load(pwd() * "/../HPC_Output/p_" * name * ".jld", "p2")
 		else
 			p = load(pwd() * "/../HPC_Output/p_" * name * ".jld", "p")
