10fd82a4e5a86eeb498f0d6c74856f1d0764b4ff
qurufinwe <fr673@nyu.edu>
2018-08-21 14:55:48 -0400

    Params

--- reiter.jl
+++ reiter.jl
@@ -2,10 +2,10 @@ using QuantEcon, BasisMatrices, Interpolations, Optim, MINPACK, LaTeXStrings, Di
 
 include("hh_pb.jl")
 
-function Hank(;	β = (1.0/1.035)^0.25,
+function Hank(;	β = (1.0/1.03)^0.25,
 				IES = 1.0,
 				RRA = 10.,
-				γw = 0.9,
+				γw = 0.99,
 				τ = 0.25,
 				r_star = 1.02^0.25 - 1.0,
 				ωmax = 20.,
