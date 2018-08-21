5ad71ad6f4dfb29f5a1114fd2c8f689bb8f5fe06
qurufinwe <fr673@nyu.edu>
2018-08-21 14:24:35 -0400

    Increased risk aversion

--- reiter.jl
+++ reiter.jl
@@ -4,7 +4,7 @@ include("hh_pb.jl")
 
 function Hank(;	β = (1.0/1.035)^0.25,
 				IES = 1.0,
-				RRA = 5.,
+				RRA = 10.,
 				γw = 0.9,
 				τ = 0.25,
 				r_star = 1.02^0.25 - 1.0,
@@ -397,7 +397,7 @@ function update_fiscalrules!(h::Hank)
 	net_iss = [ ones(unemp) unemp unemp2 spread spr2 NX NX2 ] * coef_B / 100
 
 	h.spending = max.(min.(g, 0.35), 0.) .* h.output
-	h.issuance = min.(0.1,max.(0., net_iss)) .* h.output + (1.-h.ρ)*h.bgrid[h.Jgrid[:, 1]]
+	h.issuance = min.(0.15,max.(0., net_iss)) .* h.output + (1.-h.ρ)*h.bgrid[h.Jgrid[:, 1]]
 
 	def_states = h.ζgrid[h.Jgrid[:, 5]] .!= 1.0
 	h.issuance[def_states] = (1.-h.ρ) * h.bgrid[h.Jgrid[def_states, 1]]
