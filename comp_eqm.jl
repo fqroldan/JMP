c83ba5b735592ea21af17eb052acd6de561e80be
qurufinwe <fr673@nyu.edu>
2018-08-31 11:11:02 -0400

    Increased curvature of TFP costs of default

--- comp_eqm.jl
+++ comp_eqm.jl
@@ -1,5 +1,5 @@
-TFP_N(z, Δ, ζ) = 1.0    * max(0, min(1, (1.0 - Δ*0.8*(ζ==2) - Δ*0.2*1.0^2   )))
-TFP_T(z, Δ, ζ) = exp(z) * max(0, min(1, (1.0 - Δ*0.8*(ζ==2) - Δ*0.2*exp(z)^2)))
+TFP_N(z, Δ, ζ) = 1.0    * max(0, min(1, (1.0 - Δ*0.*(ζ==2) - Δ*1.*1.0^2   )))
+TFP_T(z, Δ, ζ) = exp(z) * max(0, min(1, (1.0 - Δ*0.*(ζ==2) - Δ*1.*exp(z)^3)))
 
 function extend_state_space!(h::Hank, qʰ_mat, qᵍ_mat, T_mat)
 
