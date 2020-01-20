1ad94a260dbf5e71d6cb16de443cdf54d1d09402
qurufinwe <fr673@nyu.edu>
2020-01-20 17:15:39 -0500

    Decreased vfi min tolerance

--- reiter.jl
+++ reiter.jl
@@ -614,7 +614,7 @@ function vfi!(h::Hank; tol::Float64=5e-3, verbose::Bool=true, remote::Bool=true,
 			print_save("\nqᵍ between $(round(minimum(h.qᵍ),digits=4)) and $(round(maximum(h.qᵍ),digits=4)). risk-free is $(round(mean(h.qʰ),digits=4))")
 			print_save("\nspread between $(floor(Int,10000*minimum(h.spread))) bps and $(floor(Int,10000*maximum(h.spread))) bps")
 
-			h.upd_tol = max(exp(0.925*log(1+h.upd_tol))-1, 1e-6)
+			h.upd_tol = max(exp(0.925*log(1+h.upd_tol))-1, 1e-5)
 			t_old = time()
 		end
 
