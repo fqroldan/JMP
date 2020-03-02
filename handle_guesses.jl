c58cdda4d6780ed8a3e54a74e254f19ac8e2c401
qurufinwe <fr673@nyu.edu>
2020-03-02 12:43:19 -0500

    Started from previous guess

--- handle_guesses.jl
+++ handle_guesses.jl
@@ -8,7 +8,7 @@ function make_guess(nodef, noΔ, rep_agent, r_loc, tax, RRA, τ, ρz, σz, ρξ,
 		h2 = load(pwd() * "/../Output/hank_backup.jld", "h")
 		print_save("\nFound generic JLD file")
 		try
-			h2 = load(pwd() * "/../Output/run$(max(0,run_number-1))/hank.jld", "h") # set max(1, ...) to use run1 in first go
+			h2 = load(pwd() * "/../Output/run$(max(1,run_number-1))/hank.jld", "h") # set max(1, ...) to use run1 in first go
 			print_save("\nFound JLD file from last run")
 		catch
 		end
