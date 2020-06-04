01b4b4256a89149fb5b2265bbfd381f8defd416b
qurufinwe <fr673@nyu.edu>
2020-06-04 18:08:03 -0400

    More fancy tables

--- reporting_routines.jl
+++ reporting_routines.jl
@@ -40,17 +40,17 @@ function make_params_table(sd::SOEdef)
 	table = ""
 
 	td = Dict(
-		:β => ["Discount rate of HHs			", "\$ 1 / \\beta - 1 \$		", (sd.pars[:β]^(-4)-1), true],
-		:γ => ["Risk aversion					", "\$ \\gamma \$				", sd.pars[:γ], false],
-		:τ => ["Progressivity of tax schedule 	", "\$ \\tau \$					", sd.pars[:τ], true],
-		:w => ["Wage minimum 					", "\$ \\bar{w} \$				", sd.pars[:wbar], false],
-		:z => ["TFP process						", "\$ \\rho_z, \\sigma_z \$	", [sd.pars[:ρz], sd.pars[:σz]], false],
-		:ξ0=> ["Mean risk premium 				", "\$\\bar{\\xi}\$				", sd.pars[:meanξ], true],
-		:ξ => ["Risk premium AR(1)				", "\$\\rho_\\xi, \\sigma_\\xi\$", [sd.pars[:ρξ], sd.pars[:σξ]], false],
+		:β => ["Discount rate of HHs", "\$ 1 / \\beta - 1 \$		", (sd.pars[:β]^(-4)-1), true],
+		:γ => ["Risk aversion", "\$ \\gamma \$				", sd.pars[:γ], false],
+		:τ => ["Progressivity of tax schedule", "\$ \\tau \$					", sd.pars[:τ], true],
+		:w => ["Wage minimum", "\$ \\bar{w} \$				", sd.pars[:wbar], false],
+		:z => ["TFP process", "\$ \\rho_z, \\sigma_z \$	", [sd.pars[:ρz], sd.pars[:σz]], false],
+		:ξ0=> ["Mean risk premium", "\$\\bar{\\xi}\$				", sd.pars[:meanξ], true],
+		:ξ => ["Risk premium AR(1", "\$\\rho_\\xi, \\sigma_\\xi\$", [sd.pars[:ρξ], sd.pars[:σξ]], false],
 		)
 
 	for key in [:β, :γ, :τ, :w, :z, :ξ0, :ξ]
-		table *= "\n" * td[key][1] * "&" * td[key][2] * "&"
+		table *= "\n" * rpad(td[key][1],30," ") * "&" * rpad(td[key][2],30," ") * "&"
 		if key == :z || key == :ξ
 			table *= "($(@sprintf("%0.3g",td[key][3][1])), $(@sprintf("%0.3g",(td[key][3][2]))))"
 		elseif td[key][4]
@@ -71,7 +71,10 @@ function make_params_table(sd::SOEdef)
 end
 
 function make_calib_table(v_m)
-	table = ""
+
+	table = "\\begin{tabular}{@{\\extracolsep{20pt}}lcc@{}} \\toprule \n"
+
+	colnames = ["\\textbf{Target}", "\\textbf{Model}", "\\textbf{Data}"]
 
 	rownames = ["AR(1) coef \$\\log(Y_t)\$"; "Std coef \$\\log(Y_t)\$"; "AR(1) coef \$\\log(C_t)\$"; "Std coef \$\\log(C_t)\$"; "AR(1) coef spread"; "Std coef spread	"; "Avg Debt-to-GDP	"; "Std Debt-to-GDP	"; "Avg unemployment"; "Std unemployment"; "Median dom holdings"; "Avg wealth-to-GDP"]
 
@@ -80,47 +83,87 @@ function make_calib_table(v_m)
 	list_perc = ones(size(data_stats))
 	list_perc[1:6] = zeros(6)
 
-	for jj in 1:length(data_stats)
-		list_perc[jj] == 1 ? perc = "\\%" : perc = ""
-		table *= "\n		" * rownames[jj] * "	& 	$(@sprintf("%0.3g",v_m[jj]))" * perc * "	& 	$(@sprintf("%0.3g",data_stats[jj]))" * perc * "\\\\"
-	end
-	table *= "\n		\\bottomrule"
+	vs = [@sprintf("%0.3g",vv)*ifelse(list_perc[jv]==1,"\\%", "") for (jv, vv) in enumerate(v_m)]
+	ds = [@sprintf("%0.3g",dv)*ifelse(list_perc[jv]==1,"\\%", "") for (jv, dv) in enumerate(data_stats)]
 
+	pad_n = max(length(colnames[1]), maximum(length.(rownames))) + 1
+	pad_v = max(length(colnames[2]), maximum(length.( vs ))) + 3
+	pad_d = max(length(colnames[3]), maximum(length.( ds ))) + 3
+
+	table *= rpad(colnames[1],pad_n, " ") *  "& " * rpad(colnames[2], pad_v, " ") * "& " *	rpad(colnames[3], pad_d, " ") * "\\\\ \\midrule \n"
+	for jj in 1:length(data_stats)
+		table *= * rpad(rownames[jj],pad_n," ") * "& " * rpad(vs[jj], pad_v, " ") * "& "* rpad(ds[jj], pad_d, " ") * "\\\\\n"
+	end
+	table *= "\\bottomrule"
+
+	table *= " \\multicolumn{3}{@{}p{.5\\textwidth}@{}}{\\footnotesize All data from Eurostat 2000Q1:2017Q4, except private consumption from OECD 2000Q1:2017Q4, domestic holdings from Banco de España, 2004Q1:2017Q4}\n"
+
+	table *= "\\end{tabular}"
 	return table
 end
 
 function make_calib_table_comp(v_m, v_m_nodef, v_m_noΔ=[], v_m_nob=[])
-	table = "Moment 			& Benchmark "
+	k = sum(length(v) > 0 for v in [ v_m, v_m_nodef, v_m_noΔ, v_m_nob ])
+	table = "\\begin{tabular*}{.8\\textwidth}{@{\\extracolsep{\\fill}}l*{$k}c@{}} \\toprule \n"
+
+	colnames = ["\\textbf{Moment}", "\\textbf{Benchmark}"]
+	rownames = ["AR(1) coef \$\\log(Y_t)\$"; "Std coef \$\\log(Y_t)\$"; "AR(1) coef \$\\log(C_t)\$"; "Std coef \$\\log(C_t)\$"; "AR(1) coef spread"; "Std coef spread	"; "Avg Debt-to-GDP	"; "Std Debt-to-GDP	"; "Avg unemployment"; "Std unemployment"; "Median dom holdings"; "Avg wealth-to-GDP"; "Default frequency"]
+	pad_n = maximum(length.(rownames)) + 1
+
 	if length(v_m_noΔ) > 0
-		table *=  " 	& \$\\Delta=0\$"
+		push!(colnames, "\\textbf{\$\\Delta=0\$}")
 	end
 	if length(v_m_nob) > 0
-		table *=  " 	& No dom. holdings \$"
+		push!(colnames, "\\textbf{No dom. holdings}")
 	end
-	table *= "	& No default "
-	table *= "\\\\ \\midrule"
-
-	rownames = ["AR(1) coef \$\\log(Y_t)\$"; "Std coef \$\\log(Y_t)\$"; "AR(1) coef \$\\log(C_t)\$"; "Std coef \$\\log(C_t)\$"; "AR(1) coef spread"; "Std coef spread	"; "Avg Debt-to-GDP	"; "Std Debt-to-GDP	"; "Avg unemployment"; "Std unemployment"; "Median dom holdings"; "Avg wealth-to-GDP"; "Default frequency"]
-
-	data_stats = [ 0.96580506; 0.01294576; 0.96172496; 0.01663608; 0.96656486; 0.10252351; 64.57638889; 23.48323041; 15.94722222; 6.08732167; 56.49; 94.48]
+	push!(colnames, "\\textbf{No default}")
 
 	list_perc = ones(length(v_m))
 	list_perc[1:6] = zeros(6)
+	vs = [@sprintf("%0.3g",vv)*ifelse(list_perc[jv]==1,"\\%", "") for (jv, vv) in enumerate(v_m)]
+	# pad_v = max(length(colnames[2]), maximum(length.( vs ))) + 3
+	pad_v = maximum(length.( vs )) + 3
 
-	for jj in 1:length(v_m)
-		list_perc[jj] == 1 ? perc = "\\%" : perc = ""
-		table *= "\n" * rownames[jj] * "	& $(show_float(v_m[jj]))" * perc
+	pad_n = max(length(colnames[1]), maximum(length.(rownames))) + 1
+
+	table *= rpad(colnames[1],pad_n, " ") * "& " * rpad(colnames[2], pad_v, " ")
+
+	jj = 3
+	if length(v_m_noΔ) > 0
+		v_noΔ = [@sprintf("%0.3g",vv)*ifelse(list_perc[jv]==1,"\\%", "") for (jv, vv) in enumerate(v_m_noΔ)]
+		# pad_Δ = max(length(colnames[jj]), maximum(length.( v_noΔ ))) + 3
+		pad_Δ = maximum(length.( v_noΔ )) + 3
+		table *= "& " * rpad(colnames[jj], pad_Δ, " ")
+		jj += 1
+	end
+	if length(v_m_nob) > 0
+		v_nob = [@sprintf("%0.3g",vv)*ifelse(list_perc[jv]==1,"\\%", "") for (jv, vv) in enumerate(v_m_nob)]
+		# pad_b = max(length(colnames[jj]), maximum(length.( v_nob ))) + 3
+		pad_b = maximum(length.( v_nob )) + 3
+		table *= "& " * rpad(colnames[jj], pad_b, " ")
+		jj += 1
+	end
+	v_nodef = [@sprintf("%0.3g",vv)*ifelse(list_perc[jv]==1,"\\%", "") for (jv, vv) in enumerate(v_m_nodef)]
+	# pad_def = max(length(colnames[jj]), maximum(length.( v_nodef ))) + 3
+	pad_def = maximum(length.( v_nodef )) + 3
+	table *= "& " * rpad(colnames[jj], pad_def, " ")
+	
+	table *= "\\\\ \\midrule \n"
+
+
+	for jj in 1:length(rownames)-1
+		table *= rpad(rownames[jj],pad_n," ") * "& " * rpad(vs[jj], pad_v, " ")
 		if length(v_m_noΔ) > 0
-			table *=  " 	& $(show_float(v_m_noΔ[jj]))" * perc
+			table *= "& " * rpad(v_noΔ[jj], pad_Δ, " ")
 		end
 		if length(v_m_nob) > 0
-			table *=  " 	& $(show_float(v_m_nob[jj]))" * perc
+			table *= "& " * rpad(v_nob[jj], pad_b, " ")
 		end
-		table *= "	& $(show_float(v_m_nodef[jj]))" * perc
+		table *= "& " * rpad(v_nodef[jj], pad_def, " ")
+		table *= "\\\\\n"
 
-		table *= "\\\\"
 	end
-	table *= "\n		\\bottomrule"
 
+	table *= "\\bottomrule \n\\end{tabular*}"
 	return table
 end
