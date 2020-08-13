3f2fe399a83dadbb73cc4ab5baeed2edffcead2a
qurufinwe <fr673@nyu.edu>
2020-08-13 12:54:11 -0400

    Colors for panels_comp, distribution_comp()

--- plotting_routines.jl
+++ plotting_routines.jl
@@ -269,8 +269,8 @@ function scats_comp(pvb::Vector{T}, pvn::Vector{T}, tvv::Vector{Vector{Int64}}, 
 	ybench, bench_up, bench_me, bench_lo, bench_av = series_crises(pvb, tvv, key, k)
 	ynodef, nodef_up, nodef_me, nodef_lo, nodef_av = series_crises(pvn, tvv, key, k)
 
-	colbench = get(ColorSchemes.vik, 0.3)
-	colnodef = get(ColorSchemes.vik, 0.7)
+	colbench = get(ColorSchemes.corkO, 0.25)
+	colnodef = get(ColorSchemes.corkO, 0.65)
 
 	line_bench = scatter(x=(-2k:k)/4, y=f1.(bench_me), name="Benchmark", mode="lines", line_color=colbench, fill="tonexty", showlegend=(axis==1), legendgroup = 1, xaxis="x$axis", yaxis="y$axis")
 	# lb_avg = scatter(x=(-2k:k)/4, y=f1.(bench_av), name="Benchmark", mode="lines", line_color=col[fill="tonexty", 1], showlegend=(axis==1), legendgroup = 1, xaxis="x$axis", yaxis="y$axis")
@@ -732,8 +732,8 @@ function full_scats_comp(pv_bench::Vector{T}, pv_noΔ::Vector{T}, pv_nob::Vector
 	ynob, nob_up, nob_me, nob_lo, nob_av = series_crises(pv_nob, tvv, key, k)
 	ynodef, nodef_up, nodef_me, nodef_lo, nodef_av = series_crises(pv_nodef, tvv, key, k)
 
-	colbench = get(ColorSchemes.vik, 0.25)
-	colnodef = get(ColorSchemes.vik, 0.75)
+	colbench = get(ColorSchemes.corkO, 0.25)
+	colnodef = get(ColorSchemes.corkO, 0.65)
 	colnoΔ   = get(ColorSchemes.cork, 1)
 	colnob   = get(ColorSchemes.vikO, 1)
 
