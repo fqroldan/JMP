2414ed580620ba31635643469642468759f9a9df
qurufinwe <fr673@nyu.edu>
2020-09-16 10:04:38 -0400

    Added k_back methods for panels

--- plotting_routines.jl
+++ plotting_routines.jl
@@ -235,11 +235,9 @@ function make_def_incentive(sd::SOEdef; style::Style=slides_def)
 end
 
 scats_crises(pv::Vector{T}, tvv::Vector{Vector{Int64}}, key::Symbol, X::Float64) where T <: AbstractPath = scats_crises(pv, tvv, key, x->x/X)
-function scats_crises(pv::Vector{T}, tvv::Vector{Vector{Int64}}, key::Symbol, f::Function=identity; axis::Int64=1, indiv=false, k=8, symmetric=false) where T <: AbstractPath
+function scats_crises(pv::Vector{T}, tvv::Vector{Vector{Int64}}, key::Symbol, f::Function=identity; axis::Int64=1, indiv=false, k=8, k_back=k) where T <: AbstractPath
 
-	k_back = 2k - symmetric*k
-
-	ymat, y_up, y_me, y_lo, y_av = series_crises(pv, tvv, key, k, symmetric=symmetric)
+	ymat, y_up, y_me, y_lo, y_av = series_crises(pv, tvv, key, k, k_back)
 
 	line_up = scatter(x=(-k_back:k)/4, y=f(y_up),hoverinfo="skip",showlegend=false,legendgroup=3,mode="lines",line=attr(color="rgb(31,119,180)", width=0.001), xaxis="x$axis", yaxis="y$axis")
 	line_lo = scatter(x=(-k_back:k)/4, y=f(y_lo),hoverinfo="skip",showlegend=(axis==1),legendgroup=3, name="IQR",mode="lines",line=attr(color="rgb(31,119,180)",width=0.001), fill="tonexty", fillcolor="rgba(31,119,180,0.25)", xaxis="x$axis", yaxis="y$axis")
@@ -426,8 +424,8 @@ function distribution_crises(pv::Vector{T}, πthres::Float64; style::Style=slide
 end
 
 panels_defaults(pv::Vector{T}; k=8, style::Style=slides_def, yh = 0.65, indiv=false) where T<:AbstractPath = panels_crises(pv, 0.0, style=style, yh=yh, type="default", indiv=indiv, k=k)
-function panels_crises(pv::Vector{T}, πthres::Float64; style::Style=slides_def, yh = 0.65, type="highspreads", indiv=false, k=8, symmetric=false) where T<:AbstractPath
-	Nc, tvv = get_crises(pv, πthres, k, type=type)
+function panels_crises(pv::Vector{T}, thres::Float64, sym::Symbol; style::Style=slides_def, yh = 0.65, type="highspreads", indiv=false, k=8, symmetric=false, k_back=2k -k*symmetric) where T<:AbstractPath
+	Nc, tvv = get_crises(pv, thres, sym, k, k_back, type=type)
 	println("Suggested yh=0.7 for style=paper")
 	keyvec = [:z, :Y, :C, :CoY, :B, :ψ, :spread, :π, :L, :mean, :var, :P, :avgω, :p90, :G, :T]
 
@@ -459,11 +457,10 @@ function panels_crises(pv::Vector{T}, πthres::Float64; style::Style=slides_def,
 	
 	data = Vector{GenericTrace{Dict{Symbol,Any}}}(undef, 0)
 	for (jj, key) in enumerate(keyvec)
-		for scat in scats_crises(pv, tvv, key, funcvec[jj], axis=jj, indiv=indiv, k=k, symmetric=symmetric)
+		for scat in scats_crises(pv, tvv, key, funcvec[jj], axis=jj, indiv=indiv, k=k, k_back=k_back)
 			push!(data, scat)
 		end
 	end
-	k_back = 2k - symmetric*k
 
 	a = 1/4
 	b = 1/20
