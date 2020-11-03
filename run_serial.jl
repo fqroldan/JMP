3704bba52b7dcd08a951f36fed7803886e1b0725
qurufinwe <fr673@nyu.edu>
2020-11-02 19:12:36 -0500

    Added simul to remote run

--- run_serial.jl
+++ run_serial.jl
@@ -5,4 +5,6 @@ sd = load("../Output/SOEdef.jld", "sd")
 
 mpe_iter!(sd, run_number = 1)
 
+g, p_bench, _, v_m, def_freq = make_simulated_path(sd, "../Output/run1/", 20000);
+
 save("../Output/SOEdef.jld", "sd", sd)
