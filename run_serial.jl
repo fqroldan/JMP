a5b6843e007a7c3ad36376e679c9cea07e66b4e8
qurufinwe <fr673@nyu.edu>
2020-11-02 23:44:01 -0500

    Prolongued simul length

--- run_serial.jl
+++ run_serial.jl
@@ -5,7 +5,7 @@ sd = load("../Output/SOEdef.jld", "sd")
 
 mpe_iter!(sd, run_number = 1)
 
-g, p_bench, _, v_m, def_freq = make_simulated_path(sd, "../Output/run1/", 20000);
+g, p_bench, _, v_m, def_freq = make_simulated_path(sd, "../Output/run1/", 30000);
 
 for (key, val) in pars(sd)
 	print_save("$(rpad(key, 6, " ")) => $val\n")
