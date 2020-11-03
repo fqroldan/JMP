85fce1e25eb7094343eaf844549060c120147aac
qurufinwe <fr673@nyu.edu>
2020-11-02 23:43:50 -0500

    Reports

--- run_serial.jl
+++ run_serial.jl
@@ -7,4 +7,8 @@ mpe_iter!(sd, run_number = 1)
 
 g, p_bench, _, v_m, def_freq = make_simulated_path(sd, "../Output/run1/", 20000);
 
+for (key, val) in pars(sd)
+	print_save("$(rpad(key, 6, " ")) => $val\n")
+end
+
 save("../Output/SOEdef.jld", "sd", sd)
