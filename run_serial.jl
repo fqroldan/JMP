9efd9f423dc7893a0b08dd36d611c77e83182f1f
qurufinwe <fr673@nyu.edu>
2020-11-11 11:45:54 -0500

    Added nodef calculation

--- run_serial.jl
+++ run_serial.jl
@@ -7,9 +7,16 @@ update_probs!(sd)
 mpe_iter!(sd, run_number = 1)
 
 g, p_bench, _, v_m, def_freq = make_simulated_path(sd, "../Output/run1/", 30000);
+Wr = mean([mean(series(p, :Wr)) for p in p_bench])
 
 for (key, val) in pars(sd)
 	print_save("$(rpad(key, 6, " ")) => $val\n")
 end
 
-save("../Output/SOEdef.jld", "sd", sd, "g", g, "pp", p_bench, "pars", pars(sd))
+save("../Output/SOEdef.jld", "sd", sd, "g", g, "pp", p_bench, "pars", pars(sd), "Wr", Wr)
+
+mpe_iter!(sd, run_number = 2, nodef = true)
+g, p_nodef, _, v_m, def_freq = make_simulated_path(sd, "../Output/run1/", 30000);
+Wr = mean([mean(series(p, :Wr)) for p in p_nodef])
+
+save("../Output/SOEdef_nodef.jld", "sd", sd, "g", g, "pp", p_bench, "pars", pars(sd), "Wr", Wr)
