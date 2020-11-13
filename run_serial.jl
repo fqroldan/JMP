10a6fbe397d77e33034f146e77c3d9253eacd284
qurufinwe <fr673@nyu.edu>
2020-11-13 15:16:06 -0500

    Print gr[:z] in all cases

--- run_serial.jl
+++ run_serial.jl
@@ -14,6 +14,7 @@ for (key, val) in pars(sd)
 end
 
 save("../Output/SOEdef.jld", "sd", sd, "g", g, "pp", p_bench, "pars", pars(sd), "Wr", Wr)
+print_save("$(sd.gr[:z])\n")
 
 sd2 = load("../Output/SOEdef_nodef.jld", "sd")
 for (key, val) in pars(sd)
@@ -23,5 +24,7 @@ update_probs!(sd2)
 mpe_iter!(sd2, run_number = 2, nodef = true)
 g, p_nodef, _, v_m, def_freq = make_simulated_path(sd2, "../Output/run1/", 30000);
 Wr = mean([mean(series(p, :Wr)) for p in p_nodef])
+
+print_save("$(sd2.gr[:z])\n")
 
 save("../Output/SOEdef_nodef.jld", "sd", sd2, "g", g, "pp", p_bench, "pars", pars(sd2), "Wr", Wr)
