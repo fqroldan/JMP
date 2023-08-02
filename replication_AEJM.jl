49c35206cbba2242e6f687d79e31069865c7aeea
qurufinwe <fr673@nyu.edu>
2023-08-02 10:24:54 -0400

    renumbering

--- replication_AEJM.jl
+++ replication_AEJM.jl
@@ -77,8 +77,8 @@ function replicate(folder = "../Replication/"; loaddir = "../Output/", datadir =
     fig2 = make_nw(loaddir = datadir, with_annot = false, slides = false)
     savefig(fig2, folder * "networth_ALN_SPA.pdf", width = 900, height = 300)
 
-    fig23_app = make_nw_levels(;levels = true, slides = false)
-    savefig(fig23_app, folder * "networth_levels.pdf", width = 900, height = 350)
+    fig24_app = make_nw_levels(;levels = true, slides = false)
+    savefig(fig24_app, folder * "networth_levels.pdf", width = 900, height = 350)
 
     # Table 1: Correlation of Spreads and Macroeconomic Outcomes
     regs_sec2(df_all, savedir = folder)
@@ -89,8 +89,8 @@ function replicate(folder = "../Replication/"; loaddir = "../Output/", datadir =
     savefig(fig3, folder * "minimal_tfp_d.pdf", width = 900, height = 300)
 
     sm_norep = SOEmin(ξ_d = 0)
-    fig15_app = makeplots_minimal(sm_norep, move="d", slides = false)
-    savefig(fig15_app, folder * "minimal_tfp_d_norep.pdf", width=900, height=300)
+    fig16_app = makeplots_minimal(sm_norep, move="d", slides = false)
+    savefig(fig16_app, folder * "minimal_tfp_d_norep.pdf", width=900, height=300)
 
     # Figure 4: Anticipation of redistribution in case of default
     fig4 = minimal_twoagents(slides = false)
@@ -157,46 +157,46 @@ function replicate(folder = "../Replication/"; loaddir = "../Output/", datadir =
     fig13 = panels_IRF(pIRF_bench, pIRF_nodef, pIRF_samep, cond_Y = 0.95, slides = false)
     savefig(fig13, folder * "defaultriskIRF_paper.pdf", width = 1100, height = 550)
 
-    fig13b = panels_IRF(pIRF_bench, pIRF_nodef, pIRF_samep, cond_Y=0.95, slides=false, give_stats=true)
-    savefig(fig13b, folder * "IRFineq_paper.pdf", width=900, height=350)
+    fig14 = panels_IRF(pIRF_bench, pIRF_nodef, pIRF_samep, cond_Y=0.95, slides=false, give_stats=true)
+    savefig(fig14, folder * "IRFineq_paper.pdf", width=900, height=350)
 
-    fig19_app = panels_IRF_wdata(pIRF_bench, pIRF_nodef, pIRF_samep, cond_Y = 0.95, slides=false)
-    savefig(fig19_app, folder * "panelsIRF_wdata_paper.pdf", width=900, height=500)
+    fig20_app = panels_IRF_wdata(pIRF_bench, pIRF_nodef, pIRF_samep, cond_Y = 0.95, slides=false)
+    savefig(fig20_app, folder * "panelsIRF_wdata_paper.pdf", width=900, height=500)
 
-    # Figure 14: Default-risk IRFs and tax progressivity
-    fig14 = panels_IRF_cs(pIRF_bench1, pIRF_hi, pIRF_lo, cond_Y = 0.95, slides=false, τ_hi=sd_hi.pars[:τ], τ_lo=sd_lo.pars[:τ], give_stats=true)
-    savefig(fig14, folder * "/panels_IRF_cs.pdf", width = 900, height = 500)
+    # Figure 15: Default-risk IRFs and tax progressivity
+    fig15 = panels_IRF_cs(pIRF_bench1, pIRF_hi, pIRF_lo, cond_Y = 0.95, slides=false, τ_hi=sd_hi.pars[:τ], τ_lo=sd_lo.pars[:τ], give_stats=true)
+    savefig(fig15, folder * "/panels_IRF_cs.pdf", width = 900, height = 500)
 
-    # Figure 16: Value functions in the crisis
-    fig16_app = distribution_IRF(pIRF_bench, pIRF_nodef, pIRF_samep, cond_Y = 0.95, slides = false)
-    savefig(fig16_app, folder * "distribIRF_paper.pdf", width = 800, height = 400)
+    # Figure 17: Value functions in the crisis
+    fig17_app = distribution_IRF(pIRF_bench, pIRF_nodef, pIRF_samep, cond_Y = 0.95, slides = false)
+    savefig(fig17_app, folder * "distribIRF_paper.pdf", width = 800, height = 400)
 
-    # Figure 17: Subjective probabilities of default
-    fig17 = make_twisted(sd_bench, slides = false, leg = true)
-    savefig(fig17, folder * "twistedprobs_paper.pdf", width = 800, height = 350)
+    # Figure 18: Subjective probabilities of default
+    fig18 = make_twisted(sd_bench, slides = false, leg = true)
+    savefig(fig18, folder * "twistedprobs_paper.pdf", width = 800, height = 350)
 
-    # Figures 18 and 19 are above
+    # Figures 19 and 20 are above
 
-    # Figure 20: Market Clearing in the Nontradable Sector
+    # Figure 21: Market Clearing in the Nontradable Sector
     ## This one is done in TikZ on the paper itself
 
     # Table 7: Discrepancies in Simulation
     # Get values from make_simulated_path(sd_bench,...)
 
-    # Figure 21: Transfers
-    fig21 = make_panels(sd_bench, "T", slides = false, leg = false)
-    savefig(fig21, folder * "transfers_paper.pdf", width = 800, height = 250)
+    # Figure 22: Transfers
+    fig22 = make_panels(sd_bench, "T", slides = false, leg = false)
+    savefig(fig22, folder * "transfers_paper.pdf", width = 800, height = 250)
 
-    # Figure 22: Factors limiting production
-    fig22 = make_FLP(; slides = false)
-    savefig(fig22, folder * "factorsLP_paper.pdf", width = 900, height = 350)
+    # Figure 23: Factors limiting production
+    fig23 = make_FLP(; slides = false)
+    savefig(fig23, folder * "factorsLP_paper.pdf", width = 900, height = 350)
 
-    # Figure 24: Estimated Fiscal rules
+    # Figure 25: Estimated Fiscal rules
     ## Done along with table 2
 
-    # Figure 25: Interest rates in Spain
-    fig25_bor = plot_borrowing(slides = false)
-    savefig(fig25_bor, folder * "borrowingrates_paper.pdf", width = 1000, height = 500)
-    fig25_dep = plot_deposit(slides = false)
-    savefig(fig25_dep, folder * "depositrates_paper.pdf", width = 1000, height = 500)
+    # Figure 26: Interest rates in Spain
+    fig26_bor = plot_borrowing(slides = false)
+    savefig(fig26_bor, folder * "borrowingrates_paper.pdf", width = 1000, height = 500)
+    fig26_dep = plot_deposit(slides = false)
+    savefig(fig26_dep, folder * "depositrates_paper.pdf", width = 1000, height = 500)
 end
